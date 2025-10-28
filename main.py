e2c2659cec3691d64fe2ab7c79946b300584ec71
Shiniman <163452315+Shiniman@users.noreply.github.com>
2025-10-28 16:52:58 -0700

    Main File

---
+++ main.py
@@ -0,0 +1,25 @@
+# main.py
+"""
+Python Sorting Demo
+File contains 2 functions
+- sort_integers(sorts numbers in ascending order (descending=false))
+- sort_strings(sorts strings in alphabetical order (reverse=false))
+"""
+
+def sort_integers(numbers, descending=False):
+    return sorted(numbers, reverse=descending)
+
+def sort_strings(strings, reverse=False):
+    return sorted(strings, reverse=reverse)
+
+def run_sorter():
+    int_set = [4, 7, 9, 2, 1, 4]
+    str_set = ["apple", "orange", "banana", "Apple", "banana"]
+
+    print("Integer sort (ascending): ", sort_integers(int_set))
+    print("Integer sort (descending): ", sort_integers(int_set, descending=True))
+    print("String sort (alphabet): ", sort_strings(str_set))
+    print("String sort (reverse alphabet): ", sort_strings(str_set, reverse=True))
+
+if __name__ == "__main__":
+    run_sorter()
